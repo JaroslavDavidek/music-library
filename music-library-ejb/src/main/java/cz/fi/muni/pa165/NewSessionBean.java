79ecdf26fbae141cc33cf23d61f5ec4e3e6b396a
JaroslavDavidek <jaroslavdavidek@gmail.com>
2015-10-23 23:06:25 +0200

    Initial commit
    Created maven Java EE project, persistence unit and partially
    implemented Song entity.

---
+++ music-library-ejb/src/main/java/cz/fi/muni/pa165/NewSessionBean.java
@@ -0,0 +1,16 @@
+
+package cz.fi.muni.pa165;
+
+import javax.ejb.Stateless;
+import javax.ejb.LocalBean;
+
+
+@Stateless
+@LocalBean
+public class NewSessionBean {
+
+    public void businessMethod() {
+    }
+
+
+}
---
+++ music-library-ejb/src/main/java/cz/fi/muni/pa165/entity/Song.java
@@ -0,0 +1,63 @@
+package cz.fi.muni.pa165.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+/**
+ *
+ * @author jarek
+ */
+@Entity
+@Table(name="SONG")
+public class Song {
+    
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private String title;
+    
+    private int bitrate;
+    
+    private int albumPosition;
+    
+    private String commentary;
+    
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (id != null ? id.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        if (!(object instanceof Song)) {
+            return false;
+        }
+        Song other = (Song) object;
+        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "cz.fi.muni.pa165.entity.Song[ id=" + id + " ]";
+    }
+    
+}

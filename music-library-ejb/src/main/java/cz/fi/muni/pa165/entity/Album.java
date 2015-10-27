21a5086fa8256412d444eaad5778bb1b7420398c
Dorengar <peter.franekpf@gmail.com>
2015-10-27 11:06:16 +0100

    Implemented Album entity, added empty Musician class

--- music-library-ejb/src/main/java/cz/fi/muni/pa165/entity/Album.java
+++ music-library-ejb/src/main/java/cz/fi/muni/pa165/entity/Album.java
@@ -1,6 +1,108 @@
 package cz.fi.muni.pa165.entity;
 
+import java.util.Objects;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToOne;
+import javax.validation.constraints.NotNull;
+import org.eclipse.persistence.jpa.jpql.parser.DateTime;
+
 public class Album  {
     
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    
+    @NotNull
+    private String title;
+    @OneToOne
+    @NotNull
+    private Musician musician;
+    private DateTime releaseDate;
+    private String commentary;
+    private Byte[] cover;
+
+    public Album() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public Musician getMusician() {
+        return musician;
+    }
+
+    public void setMusician(Musician musician) {
+        this.musician = musician;
+    }
+
+    public DateTime getReleaseDate() {
+        return releaseDate;
+    }
+
+    public void setReleaseDate(DateTime releaseDate) {
+        this.releaseDate = releaseDate;
+    }
+
+    public String getCommentary() {
+        return commentary;
+    }
+
+    public void setCommentary(String commentary) {
+        this.commentary = commentary;
+    }
+
+    public Byte[] getCover() {
+        return cover;
+    }
+
+    public void setCover(Byte[] cover) {
+        this.cover = cover;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 89 * hash + Objects.hashCode(this.title);
+        hash = 89 * hash + Objects.hashCode(this.musician);
+        hash = 89 * hash + Objects.hashCode(this.releaseDate);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Album other = (Album) obj;
+        if (!Objects.equals(this.title, other.title)) {
+            return false;
+        }
+        if (!Objects.equals(this.musician, other.musician)) {
+            return false;
+        }
+        if (!Objects.equals(this.releaseDate, other.releaseDate)) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "Album{" + "title=" + title + ", musician=" + musician + ", releaseDate=" + releaseDate + '}';
+    }
     
 }

7d288ef128b43b78ce7da3130fed26ee5c3257b5
JJ <JJ@DESKTOP-26LUDSB>
2015-10-30 00:23:46 +0100

    Musician entity checkout

---
+++ music-library-ejb/src/main/java/cz/fi/muni/pa165/entity/Musician.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cz.fi.muni.pa165.entity;
+
+import java.util.Objects;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.validation.constraints.NotNull;
+/**
+ *
+ * @author JJ
+ */
+public class Musician {
+    
+}

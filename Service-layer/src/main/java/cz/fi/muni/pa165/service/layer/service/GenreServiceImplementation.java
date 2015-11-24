f6e815a728dc951fd34248f0b1b177a60b90b263
jerggi <jerggi@gmail.com>
2015-11-25 00:14:23 +0100

    Genre Service implementation added to service layer

---
+++ Service-layer/src/main/java/cz/fi/muni/pa165/service/layer/service/GenreServiceImplementation.java
@@ -0,0 +1,66 @@
+package cz.fi.muni.pa165.service.layer.service;
+
+import cz.fi.muni.pa165.dao.GenreDao;
+import cz.fi.muni.pa165.entity.Genre;
+import java.util.List;
+import javax.inject.Inject;
+
+/**
+ *
+ * @author Jergus Fasanek
+ */
+public class GenreServiceImplementation implements GenreService{
+    
+    @Inject
+    private GenreDao genreDao;
+    
+    @Override
+    public Genre createGenre(Genre genre) {
+        if(genreDao.create(genre)){
+            return genre;
+        }
+        return null;
+    }
+
+    @Override
+    public boolean deleteGenre(Genre genre) {
+        if(genre != null){
+            return genreDao.delete(genre);
+        }
+        return false;
+    }
+
+    @Override
+    public Genre updateTitle(Genre genre, String newTitle) {
+        if(genre != null){
+            genre.setTitle(newTitle);
+            return genreDao.update(genre);
+        }
+        return null;
+    }
+
+    @Override
+    public Genre updateYearOfOrigin(Genre genre, int newYearOfOrigin) {
+        if(genre != null){
+            genre.setYearOfOrigin(newYearOfOrigin);
+            return genreDao.update(genre);
+        }
+        return null;
+    }
+
+    @Override
+    public Genre findGenreByID(Long genreID) {
+        return genreDao.findById(genreID);
+    }
+
+    @Override
+    public Genre findGenreByTitle(String genreTitle) {
+        return genreDao.findByTitle(genreTitle);
+    }
+
+    @Override
+    public List<Genre> findAll() {
+        return genreDao.findAll();
+    }
+    
+}

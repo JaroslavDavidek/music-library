dab2cdee32217567f449eb6c21ce11493fded831
jerggi <jerggi@gmail.com>
2015-11-25 10:45:18 +0100

    Genre Facade added to facade layer

---
+++ API-layer/src/main/java/cz/fi/muni/pa165/api/layer/facade/GenreFacade.java
@@ -0,0 +1,12 @@
+package cz.fi.muni.pa165.api.layer.facade;
+
+import cz.fi.muni.pa165.api.layer.dto.GenreDTO;
+import java.util.List;
+
+/**
+ *
+ * @author Jergus Fasanek
+ */
+public interface GenreFacade {
+    
+}
